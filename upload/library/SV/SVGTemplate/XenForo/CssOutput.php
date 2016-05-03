817ca7631e3b8e83b1acd82506986ca6c2ecd059
unknown <marcus.knight@zetpc2804.zettaserve.com>
2016-05-03 12:37:13 +0800

    Perform template replaces after render for compatibility with CSS compatibility rules.

--- upload/library/SV/SVGTemplate/XenForo/CssOutput.php
+++ upload/library/SV/SVGTemplate/XenForo/CssOutput.php
@@ -4,13 +4,17 @@ class SV_SVGTemplate_XenForo_CssOutput extends XFCP_SV_SVGTemplate_XenForo_CssOu
 {
     public function renderCss()
     {
-        $output = parent::renderCss();
-        $output = preg_replace_callback("/{xen\:helper\s+svg\s*\,\s*'([^']+)'\s*}/siUx", array('self', '_handleSvgHelperReplacement'), $output);
-        return $output;
+        XenForo_CodeEvent::addListener('template_post_render', array(__class__, 'templateRender'));
+        return parent::renderCss();
     }
 
     public static function _handleSvgHelperReplacement(array $match)
     {
          return SV_SVGTemplate_Helpers::helperSvg($match[1]);
+    }
+
+    public static function templateRender($templateName, &$content, array &$containerData, XenForo_Template_Abstract $template)
+    {
+         $content = preg_replace_callback("/{xen\:helper\s+svg\s*\,\s*'([^']+)'\s*}/siUx", array(__class__, '_handleSvgHelperReplacement'), $content);
     }
 }
